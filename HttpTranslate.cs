8039db945c98c6319179d74c31c88fc5ae36f984
Evgeny Gunko <evgeny.gunko@gmail.com>
2024-06-30 19:25:52 +0000

    Merged PR 286: Integrate Azure.AI.Translation.Text.TextTranslationClient

--- HttpTranslate.cs
+++ HttpTranslate.cs
@@ -1,12 +1,15 @@
 using System.Net;
+using Azure.AI.Translation.Text;
+using Azure;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace My.Function
 {
     public record TranslationInput(string HeadWord, string Meaning);
-    public record TranslationOutput(string HeadWord, string Meaning);
+    public record TranslationOutput(string? HeadWord, string? Meaning);
 
     public class HttpTranslate
     {
@@ -23,15 +26,41 @@ namespace My.Function
         {
             _logger.LogInformation($"Will translate '{translationInput.HeadWord}'");
 
+            try
+            {
+                string key = Environment.GetEnvironmentVariable("TRANSLATIONS_APP_KEY");
+                string region = Environment.GetEnvironmentVariable("TRANSLATIONS_APP_REGION");
+                AzureKeyCredential credential = new(key);
+                TextTranslationClient client = new(credential, region);
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
+                string? translatedHeadWord = await TranslateAsync(client, translationInput.HeadWord);
+                string? translatedMeaning = await TranslateAsync(client, translationInput.Meaning);
 
-            var translationOutput = new TranslationOutput(
-                $"TODO: {translationInput.HeadWord}",
-                $"TODO: {translationInput.Meaning}");
-            await response.WriteAsJsonAsync(translationOutput);
+                var response = req.CreateResponse(HttpStatusCode.OK);
 
-            return response;
+                var translationOutput = new TranslationOutput(translatedHeadWord, translatedMeaning);
+                await response.WriteAsJsonAsync(translationOutput);
+
+                _logger.LogInformation($"Returning translations: " + JsonSerializer.Serialize(translationOutput));
+                return response;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error occurred while calling translator API.");
+                throw;
+            }
+        }
+
+        private async Task<string?> TranslateAsync(TextTranslationClient client, string inputText)
+        {
+            string sourceLanguage = "da";
+            string targetLanguage = "en";
+
+            Response<IReadOnlyList<TranslatedTextItem>> response = await client.TranslateAsync(targetLanguage, inputText, sourceLanguage).ConfigureAwait(false);
+            IReadOnlyList<TranslatedTextItem> translations = response.Value;
+            TranslatedTextItem? translation = translations.FirstOrDefault();
+
+            return translation?.Translations?.FirstOrDefault()?.Text;
         }
     }
 }
