7d0e0aa557eabaaefb0fd967542204e291603286
Evgeny Gunko <evgeny.gunko@gmail.com>
2024-11-17 11:55:43 +0000

    Merged PR 369: Upgrade to .net 8
    Upgrade to .net 8

--- source/TranslationFunc/Program.cs
+++ source/TranslationFunc/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Hosting;
 using TranslationsFunc.Services;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureFunctionsWebApplication()
     .ConfigureServices(services =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
